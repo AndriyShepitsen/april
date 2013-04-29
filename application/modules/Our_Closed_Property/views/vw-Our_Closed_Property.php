5b3b578b50046661de371c6251dad5e9a9bdfd46
Ivan Bohun <drivanbohun@gmail.com>
2013-04-29 13:03:30 -0500

    Added our_partners and About_Us

--- application/modules/Our_Closed_Property/views/vw-Our_Closed_Property.php
+++ application/modules/Our_Closed_Property/views/vw-Our_Closed_Property.php
@@ -3,6 +3,32 @@
 
 
   <div id="content">
-<h1>Our_Closed_Property</h1> 
-    
+<h2>Our Sold Property</h2> 
+    <p>1911 to 1917 W. Howard St. & 1919 to 1923 W. Howard St. Evanston. 
+    11 unit and 14 unit apartment buildings Sold for $1,500,000.</p>
+
+<p>300 Glen Ellyn Rd. Bloomingdale (DuPage Co.) 5,000 sq. ft. strip Center. Sold for $820,000.</p>
+
+<p>7305 Waukegan Rd. Niles. 
+	<br/>
+“Ozzie’s” sports bar (vacant-bank owned OREO) 4,000 SQ.FT. on 35,000 square feet Sold  $460,000</p>
+
+<p>8814 Niles Center Rd. Skokie. <br/>
+A seven year old two story Elevator 7,000 sq.ft office bld. (vacant bank OREO) sold for $685,000</p>
+
+<p>5121 to 5129 W. Dempster St. Skokie. <br/>
+A vacant older commercial  Set of 5 strip stores. Sold for $350,000.</p>
+<p>5301 W. Touchy Ave., Skokie, Illinois <br/>
+10,000 Square foot, two story office (6000 sq. ft.) and warehouse (4,000 sq. ft.) building. Bank Foreclosure. $530,000</p>
+
+<p>4911 West Dempster St., Skokie, Illinois <br/>
+4,500 Square foot retail building with a 4,500 square foot basement. Bank Foreclosure (former Lee’s Auto Parts). Curent owner has done extensive remodeling, building now has two leased stores. $600,000</p>
+
+<p>1775 Maple St., Northbrook, Illinois 3,200Square foot single story office building. Bank owned. Sold for $370,000.</p>
+
+<p>Rapids Mall shopping Center, Wisconsin<br/>
+160,000 Square foot shopping center with 34 stores. Sold for $3,200,000</p>
+
+<p>5140 W. Dempster St., Skokie, IllinoisFormer BP gas station at corner of Dempster/Gross Point Rd. Land sold for $975,000
+and rebuilt as a new Shell gas station with two retail stores.</p>
     </div>
--- application/modules/Our_Partners/views/vw-Our_Partners.php
+++ application/modules/Our_Partners/views/vw-Our_Partners.php
@@ -3,6 +3,36 @@
 
  <div id="content">
 
-<h1>Our Partners</h1> 
-    
-    </div>
+<h2>Our Partners</h2> 
+    We have worked with many banks, including:
+
+<p>AIG</p>
+<p>ASC - American Services Company</p>
+<p>Aurora Loan Services</p>
+<p>Banco Popular</p>
+<p>Bank of America / Country Wide</p>
+<p>Bayview Loan Servicing</p>
+<p>Citibank / CitGroup</p>
+<p>EMC Mortgage</p>
+<p>EverHome</p>
+<p>Fifth Third Bank</p>
+<p>First Franklin Loan Services</p>
+<p>Harris Bank</p>
+<p>HFC / Household Bank</p>
+<p>HomEq Servicing</p>
+<p>HSBC</p>
+<p>ING Bank</p>
+<p>JP Morgan Chase / Wacovia</p>
+<p>Litton Loan Servicing</p>
+<p>Nation Star Mortgage</p>
+<p>Ocwen Mortgage</p>
+<p>Option One Mortgage</p>
+<p>PNC Bank / National City</p>
+<p>Saxon Mortgage</p>
+<p>SunTrust Bank</p>
+<p>Taylor Bean & Whitaker</p>
+<p>TCF Bank</p>
+<p>US Bank</p>
+
+
+</div>
