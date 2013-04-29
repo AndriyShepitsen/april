85551c232143f8a0a1474d38113faeb192c9a949
Ivan Bohun <drivanbohun@gmail.com>
2013-04-29 17:05:24 -0500

    finished filling information

--- application/modules/Information_for_Buyers/views/vw-Short_Sale_for_Buyer.php
+++ application/modules/Information_for_Buyers/views/vw-Short_Sale_for_Buyer.php
@@ -2,6 +2,34 @@
 -->
 
     <div id="content">
-Short_Sale_for_Buyer
+<h2>Short Sale for Buyer</h2>
+    <hr/>
+    <p>During a short sale, homeowners agree to sell a house for less than the amount owed on the mortgage loan. This type of sale requires the lender to approve of the deal. While short sales sometimes leave sellers owing money to the lender, they also provide an alternative to foreclosure. In addition to helping the seller protect their credit, short sales offer a number of benefits to buyers.</p>
+    <h4 class="subheader">Pros of Short Sales for Buyer</h4>
+<hr/>
+<ol class="shortSaleVSForeclosure">
+  <li>Can typically purchase property 15%+ under market value</li>
+  <li>May not have to put any Earnest Money down for awhile, but still have involvement with property</li>
+  <li>Viable way to put in offers on lots of inventory and not have to close all at once; easy to fill the pipeline</li>
+  <li>Ability to perhaps have influence of purchase price of the home and negotiate worth</li>
+  <li>A lot of buyers don’t like short sales; access to inventory others won’t touch or don’t understand.</li>
+</ol>
+<h4 class="subheader">Cons of Short Sales for Buyers</h4>
+<hr/>
+<ol class="shortSaleVSForeclosure">
+
+    <li>Property may go to foreclosure due to lack of Listing Agents experience, bad approvals, etc.</li>
+    <li>Approvals may require you to hold property for 30-120 days after the sale, eating up profit</li>
+    <li>Sellers may trash house and bank will not allow you to negotiate for any concessions or price drop</li>
+    <li>Can’t typically assign the contract or wholesale a short sale</ol></li>
+
+<p>With all this being said, it’s important to approach Sellers as to HOW you can help them make their short sale easier, and encourage them to speak to attorney, as well.</p>
+
+<p>Beyond that, if they don’t already have one, suggest a well-known and experienced Listing Agent in their area that you know will handle their short sale proficiently.</p>
+
+<p>There aren’t many reasons why a foreclosure is a better option for homeowners. In many cases it boils down to apathy on their part, lack of communication, or extenuating circumstances.</p>
+
+<p>But with an experienced team, willing Seller, and amiable Lender/s, a short sale may be a foreclosure alternative that underwater homeowners can explore for their best benefit, and your boost your bottom line. </p>
     
+
     </div>
--- application/modules/Information_for_Sellers/views/vw-Short_Sale_vs_Forecloser.php
+++ application/modules/Information_for_Sellers/views/vw-Short_Sale_vs_Forecloser.php
@@ -7,8 +7,7 @@
 
 <p>As Investors and Buyers, coming to the aid of a distressed Seller is what we’re supposed to be highly skilled at. Depending on their situation, and the condition of the home, their options may become more and more limited. It may come down to not what they want, but what options they have left.</p>
 
-<p>In the case of choosing a short sale over a foreclosure, typically, the answer seems to be obvious from the beginning. If there is more debt owed on the property than what it’s worth, or what the Buyer is willing to pay, clearly the debt needs to be negotiated down if they plan to sell.
-</p>
+<p>In the case of choosing a short sale over a foreclosure, typically, the answer seems to be obvious from the beginning. If there is more debt owed on the property than what it’s worth, or what the Buyer is willing to pay, clearly the debt needs to be negotiated down if they plan to sell.</p>
 <p>But, some Sellers may just want to capitulate and walk away. If it seems like a viable lead though, you may want to educate the homeowners on the pros and cons of the decision they’re considering.</p>
 
 <p>If they’re not motivated and uncooperative, save your breath. No amount of sensibility about the deal structure will engage an uninterested party, so make sure you’re qualifying the Seller just as hard as you’re qualifying the deal.</p>
@@ -18,7 +17,7 @@
 <h4 class="subheader">Short Sale Pro’s for Homeowners</h4>
 <hr/>
 
-<ol >
+<ol class="shortSaleVSForeclosure">
   <li>Short Sales have gotten easier – may be able to complete one in a few months</li>
   <li>May not have to miss any payments</li>
   <li>May automatically qualify for a short sale (in the case of a divorce, disease, and disability)</li>
@@ -30,56 +29,39 @@
   <li>Free rent while living in home during short sale process</li>
 </ol>
 
-<h4 class="subheader">Short Sale Con’s for Homeowners<h4/>
-	<hr/>
+<h4 class="subheader">Short Sale Con’s for Homeowners</h4>
+<hr/>
+<ol class="shortSaleVSForeclosure">
+  <li>Can be frustrating if not working with seasoned professional</li>
+  <li>Bank may be able to come after Seller later for money if Lenders don’t waive deficiency rights</li>
+  <li>May lose home to foreclosure if Buyer isn’t serious and backs out (this has a lot to do with #1)</li>
+  <li>Bank may be uncooperative and turn down reasonable offers/issue approvals at inflated pricing</li>
+</ol>
+<p>In some of these cases, it may be better for the homeowner to exercise every option until the home goes to foreclosure. For instance, here in Arizona, we have strong anti-deficiency statues that protect homeowners that lose a home through Trustee Sale. In these cases, the lien holders lose their deficiency rights through the Trustees Sale, however; the anti-deficiency statutes only come into play with the foreclosure, NOT a short sale.</p>
 
-    Can be frustrating if not working with seasoned professional
-    Bank may be able to come after Seller later for money if Lenders don’t waive deficiency rights
-    May lose home to foreclosure if Buyer isn’t serious and backs out (this has a lot to do with #1)
-    Bank may be uncooperative and turn down reasonable offers/issue approvals at inflated pricing
+<p>If the debt holders on the short sale are unwilling to waive those rights on their approval letters, and the homeowner would otherwise legally be divorced of any further obligation if they let it go to foreclosure, the short sale may not be the best option. (It can be a little tricky with debt like Home Equity lines, cash out refinances, so either way the Seller will need to talk to an attorney).</p>
 
-In some of these cases, it may be better for the homeowner to exercise every option until the home goes to foreclosure. For instance, here in Arizona, we have strong anti-deficiency statues that protect homeowners that lose a home through Trustee Sale. In these cases, the lien holders lose their deficiency rights through the Trustees Sale, however; the anti-deficiency statutes only come into play with the foreclosure, NOT a short sale.
+<h4 class="subheader">Buyers Perspective – they NEED you</h4>
+<hr/>
+<p>Now where does your value add come in as a cash buyer/investor? To some effect, it’s one of the most important parts of the transaction!</p>
 
-If the debt holders on the short sale are unwilling to waive those rights on their approval letters, and the homeowner would otherwise legally be divorced of any further obligation if they let it go to foreclosure, the short sale may not be the best option. (It can be a little tricky with debt like Home Equity lines, cash out refinances, so either way the Seller will need to talk to an attorney).
-Buyers Perspective – they NEED you
+<p>You can let the Listing Agent and/or homeowner know (in fact, you may even want to create a Cover Letter for this to turn in with your Offers!)</p>
+<ol class="shortSaleVSForeclosure">
+  <li>You are a serious cash buyer that is aware of the short sale process</li>
+  <li>You wish to help make the process as easy on everyone as possible. You like being educated and would prefer to be communicated with on a weekly basis.</li>
+  <li>If the Listing Agent can provide you with the following information, it would greatly help get the transaction off on the right foot:</li>
+	<ul class="square">
+		<li>How many short sales have they done?</li>
+		<li>Who are the Lenders on this property?</li>
+		<li>Is there an HOA? If so, are they behind?</li>
+		<li>Is the Seller cooperative?</li>
+		<li>Where are they at with the process with the bank?</li>
+		<li>Have they pulled title?</li>
+		<li>Has the bank done a valuation? If so, when? For how much?</li>
+	</ul>
+</ol>
 
-Now where does your value add come in as a cash buyer/investor? To some effect, it’s one of the most important parts of the transaction!
+<p>All of this opens the dialogue for you the Buyer knowing what you’re looking to work with, and potentially save yourself months of frustration. Gather as much information from the get go and “prepare or pass” on the short sale, according to your findings.</p>
 
-You can let the Listing Agent and/or homeowner know (in fact, you may even want to create a Cover Letter for this to turn in with your Offers!)
-
-    You are a serious cash buyer that is aware of the short sale process
-    You wish to help make the process as easy on everyone as possible. You like being educated and would prefer to be communicated with on a weekly basis.
-    If the Listing Agent can provide you with the following information, it would greatly help get the transaction off on the right foot:
-        How many short sales have they done?
-        Who are the Lenders on this property?
-        Is there an HOA? If so, are they behind?
-        Is the Seller cooperative?
-        Where are they at with the process with the bank?
-        Have they pulled title?
-        Has the bank done a valuation? If so, when? For how much?
-
-All of this opens the dialogue for you the Buyer knowing what you’re looking to work with, and potentially save yourself months of frustration. Gather as much information from the get go and “prepare or pass” on the short sale, according to your findings.
-Pros of Short Sales for Buyer
-
-    Can typically purchase property 15%+ under market value
-    May not have to put any Earnest Money down for awhile, but still have involvement with property
-    Viable way to put in offers on lots of inventory and not have to close all at once; easy to fill the pipeline
-    Ability to perhaps have influence of purchase price of the home and negotiate worth
-    A lot of buyers don’t like short sales; access to inventory others won’t touch or don’t understand.
-
-Cons of Short Sales for Buyers
-
-    Property may go to foreclosure due to lack of Listing Agents experience, bad approvals, etc.
-    Approvals may require you to hold property for 30-120 days after the sale, eating up profit
-    Sellers may trash house and bank will not allow you to negotiate for any concessions or price drop
-    Can’t typically assign the contract or wholesale a short sale
-
-With all this being said, it’s important to approach Sellers as to HOW you can help them make their short sale easier, and encourage them to speak to attorney, as well.
-
-Beyond that, if they don’t already have one, suggest a well-known and experienced Listing Agent in their area that you know will handle their short sale proficiently.
-
-There aren’t many reasons why a foreclosure is a better option for homeowners. In many cases it boils down to apathy on their part, lack of communication, or extenuating circumstances.
-
-But with an experienced team, willing Seller, and amiable Lender/s, a short sale may be a foreclosure alternative that underwater homeowners can explore for their best benefit, and your boost your bottom line. 
 
     </div>
