5dff793f3b71e68482bc0e5cccbc710607096555
Ivan Bohun <drivanbohun@gmail.com>
2013-04-29 14:58:13 -0500

    added info

--- application/modules/Information_for_Sellers/views/vw-Short_Sale_vs_Forecloser.php
+++ application/modules/Information_for_Sellers/views/vw-Short_Sale_vs_Forecloser.php
@@ -2,6 +2,84 @@
 -->
 
   <div id="content">
-<h1>vw-Short_Sale_vs_Forecloser</h1> 
-    
+<h2>Short Sale vs Foreclosure</h2> 
+    <hr/>
+
+<p>As Investors and Buyers, coming to the aid of a distressed Seller is what we’re supposed to be highly skilled at. Depending on their situation, and the condition of the home, their options may become more and more limited. It may come down to not what they want, but what options they have left.</p>
+
+<p>In the case of choosing a short sale over a foreclosure, typically, the answer seems to be obvious from the beginning. If there is more debt owed on the property than what it’s worth, or what the Buyer is willing to pay, clearly the debt needs to be negotiated down if they plan to sell.
+</p>
+<p>But, some Sellers may just want to capitulate and walk away. If it seems like a viable lead though, you may want to educate the homeowners on the pros and cons of the decision they’re considering.</p>
+
+<p>If they’re not motivated and uncooperative, save your breath. No amount of sensibility about the deal structure will engage an uninterested party, so make sure you’re qualifying the Seller just as hard as you’re qualifying the deal.</p>
+
+<p>If it’s a matter of needing to be educated to make a decision though, let’s explore when a short sale may be better for them over when it might be better for them to walk away. </p>
+
+<h4 class="subheader">Short Sale Pro’s for Homeowners</h4>
+<hr/>
+
+<ol >
+  <li>Short Sales have gotten easier – may be able to complete one in a few months</li>
+  <li>May not have to miss any payments</li>
+  <li>May automatically qualify for a short sale (in the case of a divorce, disease, and disability)</li>
+  <li>Will not have to fix up or do anything to the house</li>
+  <li>May be better on their credit</li>
+  <li>May be able to get moving money from the bank $3,000-$30,000</li>
+  <li>May not have to pay taxes on forgiven debt</li>
+  <li>May not owe anything after sale for difference of what was owed vs sales price</li>
+  <li>Free rent while living in home during short sale process</li>
+</ol>
+
+<h4 class="subheader">Short Sale Con’s for Homeowners<h4/>
+	<hr/>
+
+    Can be frustrating if not working with seasoned professional
+    Bank may be able to come after Seller later for money if Lenders don’t waive deficiency rights
+    May lose home to foreclosure if Buyer isn’t serious and backs out (this has a lot to do with #1)
+    Bank may be uncooperative and turn down reasonable offers/issue approvals at inflated pricing
+
+In some of these cases, it may be better for the homeowner to exercise every option until the home goes to foreclosure. For instance, here in Arizona, we have strong anti-deficiency statues that protect homeowners that lose a home through Trustee Sale. In these cases, the lien holders lose their deficiency rights through the Trustees Sale, however; the anti-deficiency statutes only come into play with the foreclosure, NOT a short sale.
+
+If the debt holders on the short sale are unwilling to waive those rights on their approval letters, and the homeowner would otherwise legally be divorced of any further obligation if they let it go to foreclosure, the short sale may not be the best option. (It can be a little tricky with debt like Home Equity lines, cash out refinances, so either way the Seller will need to talk to an attorney).
+Buyers Perspective – they NEED you
+
+Now where does your value add come in as a cash buyer/investor? To some effect, it’s one of the most important parts of the transaction!
+
+You can let the Listing Agent and/or homeowner know (in fact, you may even want to create a Cover Letter for this to turn in with your Offers!)
+
+    You are a serious cash buyer that is aware of the short sale process
+    You wish to help make the process as easy on everyone as possible. You like being educated and would prefer to be communicated with on a weekly basis.
+    If the Listing Agent can provide you with the following information, it would greatly help get the transaction off on the right foot:
+        How many short sales have they done?
+        Who are the Lenders on this property?
+        Is there an HOA? If so, are they behind?
+        Is the Seller cooperative?
+        Where are they at with the process with the bank?
+        Have they pulled title?
+        Has the bank done a valuation? If so, when? For how much?
+
+All of this opens the dialogue for you the Buyer knowing what you’re looking to work with, and potentially save yourself months of frustration. Gather as much information from the get go and “prepare or pass” on the short sale, according to your findings.
+Pros of Short Sales for Buyer
+
+    Can typically purchase property 15%+ under market value
+    May not have to put any Earnest Money down for awhile, but still have involvement with property
+    Viable way to put in offers on lots of inventory and not have to close all at once; easy to fill the pipeline
+    Ability to perhaps have influence of purchase price of the home and negotiate worth
+    A lot of buyers don’t like short sales; access to inventory others won’t touch or don’t understand.
+
+Cons of Short Sales for Buyers
+
+    Property may go to foreclosure due to lack of Listing Agents experience, bad approvals, etc.
+    Approvals may require you to hold property for 30-120 days after the sale, eating up profit
+    Sellers may trash house and bank will not allow you to negotiate for any concessions or price drop
+    Can’t typically assign the contract or wholesale a short sale
+
+With all this being said, it’s important to approach Sellers as to HOW you can help them make their short sale easier, and encourage them to speak to attorney, as well.
+
+Beyond that, if they don’t already have one, suggest a well-known and experienced Listing Agent in their area that you know will handle their short sale proficiently.
+
+There aren’t many reasons why a foreclosure is a better option for homeowners. In many cases it boils down to apathy on their part, lack of communication, or extenuating circumstances.
+
+But with an experienced team, willing Seller, and amiable Lender/s, a short sale may be a foreclosure alternative that underwater homeowners can explore for their best benefit, and your boost your bottom line. 
+
     </div>
--- application/modules/Our_Partners/views/vw-Our_Partners.php
+++ application/modules/Our_Partners/views/vw-Our_Partners.php
@@ -4,35 +4,35 @@
  <div id="content">
 
 <h2>Our Partners</h2> 
-    We have worked with many banks, including:
+   <h4 > We have worked with many banks, including:</h4>
 
-<p>AIG</p>
-<p>ASC - American Services Company</p>
-<p>Aurora Loan Services</p>
-<p>Banco Popular</p>
-<p>Bank of America / Country Wide</p>
-<p>Bayview Loan Servicing</p>
-<p>Citibank / CitGroup</p>
-<p>EMC Mortgage</p>
-<p>EverHome</p>
-<p>Fifth Third Bank</p>
-<p>First Franklin Loan Services</p>
-<p>Harris Bank</p>
-<p>HFC / Household Bank</p>
-<p>HomEq Servicing</p>
-<p>HSBC</p>
-<p>ING Bank</p>
-<p>JP Morgan Chase / Wacovia</p>
-<p>Litton Loan Servicing</p>
-<p>Nation Star Mortgage</p>
-<p>Ocwen Mortgage</p>
-<p>Option One Mortgage</p>
-<p>PNC Bank / National City</p>
-<p>Saxon Mortgage</p>
-<p>SunTrust Bank</p>
-<p>Taylor Bean & Whitaker</p>
-<p>TCF Bank</p>
-<p>US Bank</p>
+<h5 class="subheader">AIG</h5>
+<h5 class="subheader">ASC - American Services Comh5any</h5>
+<h5 class="subheader">Aurora Loan Services</h5>
+<h5 class="subheader">Banco h5oh5ular</h5>
+<h5 class="subheader">Bank of America / Country Wide</h5>
+<h5 class="subheader">Bayview Loan Servicing</h5>
+<h5 class="subheader">Citibank / CitGrouh5</h5>
+<h5 class="subheader">EMC Mortgage</h5>
+<h5 class="subheader">EverHome</h5>
+<h5 class="subheader">Fifth Third Bank</h5>
+<h5 class="subheader">First Franklin Loan Services</h5>
+<h5 class="subheader">Harris Bank</h5>
+<h5 class="subheader">HFC / Household Bank</h5>
+<h5 class="subheader">HomEq Servicing</h5>
+<h5 class="subheader">HSBC</h5>
+<h5 class="subheader">ING Bank</h5>
+<h5 class="subheader">Jh5 Morgan Chase / Wacovia</h5>
+<h5 class="subheader">Litton Loan Servicing</h5>
+<h5 class="subheader">Nation Star Mortgage</h5>
+<h5 class="subheader">Ocwen Mortgage</h5>
+<h5 class="subheader">Oh5tion One Mortgage</h5>
+<h5 class="subheader">h5NC Bank / National City</h5>
+<h5 class="subheader">Saxon Mortgage</h5>
+<h5 class="subheader">SunTrust Bank</h5>
+<h5 class="subheader">Taylor Bean & Whitaker</h5>
+<h5 class="subheader">TCF Bank</h5>
+<h5 class="subheader">US Bank</h5>
 
 
-</div>
+</d class="subheader"iv>
