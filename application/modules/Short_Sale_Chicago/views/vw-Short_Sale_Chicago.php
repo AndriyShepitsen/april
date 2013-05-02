9349114f8a83bd6913eb8adcadb01fdc1e0d56bb
Ivan Bohun <drivanbohun@gmail.com>
2013-05-02 13:21:14 -0500

    Short_Sale_Chicago style

--- application/modules/Short_Sale_Chicago/views/vw-Short_Sale_Chicago.php
+++ application/modules/Short_Sale_Chicago/views/vw-Short_Sale_Chicago.php
@@ -1,28 +1,32 @@
 
 <!-- //LOCATION: april/
 -->
-    <div id="content">
-   <section id="shortSaleRealtors"> 
-   	<h2>Short Sale Realtors</h2>
+<div id="content">
+ <section id="shortSaleRealtors"> 
+    <div class="welcome">
+        <h3> Welcome to RE/MAX Short Sale Chicago!</h3>
+    </div>
     <hr/>
-    <p>RE/MAX Short Sale Chicago has the knowledge, experience and expertise to guide homeowners in a pre-foreclosure situation and help choose if a Short Sale is the best choice for you. 
+    <div class="homePageText"><p>RE/MAX Short Sale Chicago has the knowledge, experience and expertise to guide homeowners in a pre-foreclosure situation and help choose if a Short Sale is the best choice for you. 
+    
+            One time approved, a Short Sale helps an owner avoid foreclosure, additional fees paid to the lender for costs associated with a foreclosure, long term affects to their credit document and far more.</p></div>
+        <div class="avoidForeclosure">  
+            <p><strong>We can help you to avoid foreclosure!</strong> <br/>In case you know you can no longer afford your home and can not sell your home because it is worth less than you owe, a Short Sale is your best choice.</p>
+        </div>
         
-    One time approved, a Short Sale helps an owner avoid foreclosure, additional fees paid to the lender for costs associated with a foreclosure, long term affects to their credit document and far more.</p>
-       
-    <p>Avoiding foreclosure is always the best choice. In case you know you can no longer afford your home and can not sell your home because it is worth less than you owe, a Short Sale is your best choice.</p>
-        
-    <p class="callUsHome">Call us for a free no obligation consultation: 312-544-00-64</p>
+
+        <p class="callUsHome">Call us for a free no obligation consultation: <br/>312-544-00-64</p>
     </section>    `
-<hr>
-    <div id="shortSaleNews">
-<img src="_img/chicago short sale banks.gif" width="407" height="349" alt="chicago short sale banks served">
+    <hr>
+    <div class="usBanks">
+        <img src="_img/chicago short sale banks.gif" width="407" height="349" alt="chicago short sale banks served">
     </div>
 
     <!--end shortSaleNews -->
-	<div id="areasServed">
-    <h1>Areas served:</h1>
+    <div id="areasServed">
+        <h1>Areas served:</h1>
+
+        <p> Addison, Arlington Heights, Algonquin, Alsip, Antioch, Aurora, Bannockburn, Barrington, Bartlett, Belvidere, Bensenville, Berwyn, Bloomingdale, Bolingbrook, Buffalo Grove, Bridgeview, Burbank, Burr Ridge, Carol Stream, Carpentersville, Cary, Cicero, Crystal Lake, Deerfield, Deer Park, Des Plaines, Downers Grove, Dundee, Edison Park, Elgin, Elk Grove Village, Elmhurst, Elmwood Park, Evanston, Fox Lake, Forest Glen, Franklin Park, Geneva, Genoa, Glencoe, Glendale Heights, Glenview, Grayslake, Great Lakes, Gurnee, Hainsville, Hampshire, Hanover Park, Harwood Heights, Hawthorn Woods, Highland Park, Hinsdale, Hoffman Estates, Huntley, Ingleside, Inverness, Island Lake, Itasca, Ivanhoe, Irving Park, Jefferson Park, Joliet, Kenilworth, Kildeer, La Grange, Lake Bluff, Lake in the Hills, Lake Forest, Lake Villa, Lakeview, Lake Zurich, Lemont, Libertyville, Lisle, Lincoln Park, Lincolnshire, Lindenhurst, Lombard, Long Grove, Marengo, McHenry, Melrose Park, Morton Grove, Mount Prospect, Mundelein, Niles, Naperville, Norridge, Northbrook, Northfield, North Center, North Chicago, Northlake, Norwood Park, North Shore, OHare, Oak Brook, Oak Lawn, Oak Park, Old Town, Orland Park, Palatine, Park Ridge, Plainfield, Prairie View, Portage Park, Prospect Heights, Richmond, River Grove, Riverwooods, Rockford, Rogers Park, Rolling Meadows, Rockford, Roselle, Round Lake, Schaumburg, Schiller Park, Skokie, Sleepy Hollow, South Barrington, South Elgin, Spring Grove, Spring Grove, St. Charles, Stone Park, Streamwood, Sugar Grove, Summit, Sycamore, Sugar Grove, Tinley Park, Vernon Hills, Villa Park, Winnetka, Wadsworth, Wauconda, Waukegan, Wayne, Westchester, Wheaton, Wheeling, Willowbrook, Wilmette, Winthrop Harbor, Wonder Lake, Wood Dale, Woodridge, Woodstock, Zion, Cook, DuPage, Lake, McHenry, Will, Kane, Kendall County, Illinois.</p>
+    </div>   
     
-    <p> Addison, Arlington Heights, Algonquin, Alsip, Antioch, Aurora, Bannockburn, Barrington, Bartlett, Belvidere, Bensenville, Berwyn, Bloomingdale, Bolingbrook, Buffalo Grove, Bridgeview, Burbank, Burr Ridge, Carol Stream, Carpentersville, Cary, Cicero, Crystal Lake, Deerfield, Deer Park, Des Plaines, Downers Grove, Dundee, Edison Park, Elgin, Elk Grove Village, Elmhurst, Elmwood Park, Evanston, Fox Lake, Forest Glen, Franklin Park, Geneva, Genoa, Glencoe, Glendale Heights, Glenview, Grayslake, Great Lakes, Gurnee, Hainsville, Hampshire, Hanover Park, Harwood Heights, Hawthorn Woods, Highland Park, Hinsdale, Hoffman Estates, Huntley, Ingleside, Inverness, Island Lake, Itasca, Ivanhoe, Irving Park, Jefferson Park, Joliet, Kenilworth, Kildeer, La Grange, Lake Bluff, Lake in the Hills, Lake Forest, Lake Villa, Lakeview, Lake Zurich, Lemont, Libertyville, Lisle, Lincoln Park, Lincolnshire, Lindenhurst, Lombard, Long Grove, Marengo, McHenry, Melrose Park, Morton Grove, Mount Prospect, Mundelein, Niles, Naperville, Norridge, Northbrook, Northfield, North Center, North Chicago, Northlake, Norwood Park, North Shore, OHare, Oak Brook, Oak Lawn, Oak Park, Old Town, Orland Park, Palatine, Park Ridge, Plainfield, Prairie View, Portage Park, Prospect Heights, Richmond, River Grove, Riverwooods, Rockford, Rogers Park, Rolling Meadows, Rockford, Roselle, Round Lake, Schaumburg, Schiller Park, Skokie, Sleepy Hollow, South Barrington, South Elgin, Spring Grove, Spring Grove, St. Charles, Stone Park, Streamwood, Sugar Grove, Summit, Sycamore, Sugar Grove, Tinley Park, Vernon Hills, Villa Park, Winnetka, Wadsworth, Wauconda, Waukegan, Wayne, Westchester, Wheaton, Wheeling, Willowbrook, Wilmette, Winthrop Harbor, Wonder Lake, Wood Dale, Woodridge, Woodstock, Zion, Cook, DuPage, Lake, McHenry, Will, Kane, Kendall County, Illinois.</p>
- </div>   
-    
-    </div>
+</div>
