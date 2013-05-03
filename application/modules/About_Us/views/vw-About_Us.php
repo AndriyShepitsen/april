be9e7d9feb0cfb97956b59050a3e6b84701622cf
Ivan Bohun <drivanbohun@gmail.com>
2013-05-03 12:32:00 -0500

    final

--- application/modules/About_Us/views/vw-About_Us.php
+++ application/modules/About_Us/views/vw-About_Us.php
@@ -1,3 +1,4 @@
+
 <!-- //LOCATION: april/About-Us 
 -->
 
@@ -14,7 +15,9 @@ Prior to purchasing RE/MAX 1-st Class we were associated with M-G-M Realty. Inc.
 <p>M-G-M’s REO/Risk Assets division has represented many  financial institutions such as Chase Bank, Bank One, 1st National Bank of Chicago, American National Bank, NBD Bank, Midwest Bank, Talman Federal Savings, LaSalle/Talman FSB, Unity Federal Savings and Loan, Northwest Federal Savings and Loan, Lyons Savings & Loan and the Ravenswood Bank.</p>
 
 <p>Throughout our career, we have participated in the sales of troubled  real estate assets, Investment Syndication,  Various Shopping Center Developments and Management, Condominimum development and conversions, Sales of investment and Commercial Properties, and Construction of single family homes, townhouses and duplexes.</p>
-<p>The following characteristics place out team a step above other Professional Realtors:</p>
+<div class="aboutUsLable">	<h2>The following characteristics place out team a step above other Professional Realtors:</h2>
+</div>
+<div class="ourTeam">
 <ul class="disc">
       <li>Excellent knowledge of both marketing and appraisals</li>
       <li>Extensive national and international client database</li>
@@ -22,7 +25,7 @@ Prior to purchasing RE/MAX 1-st Class we were associated with M-G-M Realty. Inc.
       <li>Knowledge of local building requirements and county codes</li>
       <li>Association with construction and trade professionals</li>
     </ul>
-
+</div>
 
 </div>
 <script type="text/javascript">
--- application/modules/Short_Sale_Chicago/views/vw-Short_Sale_Chicago.php
+++ application/modules/Short_Sale_Chicago/views/vw-Short_Sale_Chicago.php
@@ -1,4 +1,5 @@
 
+
 <!-- //LOCATION: april/
 -->
 <div id="content">
